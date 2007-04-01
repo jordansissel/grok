4de103e1f10252c243bb3c73143ac531e6f79fad
Jordan Sissel <jls@semicomplete.com>
2007-04-01 02:13:23 +0000

    sigh
    git-svn-id: https://semicomplete.googlecode.com/svn/grok@1346 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- t/theory/testfork.pl
+++ t/theory/testfork.pl
@@ -5,22 +5,21 @@ use warnings;
 
 use re 'eval';
 use IO::Handle;
+use Socket;
 
 my $re = qr/(\b\w+\b)/;
 my $subre = qr/bar/;
 my $stopre = '(?=.\A)';
 
-pipe(RPCREAD, RPCWRITE);
-pipe(RESULTREAD, RESULTWRITE);
-
-RPCWRITE->autoflush(1);
-RESULTWRITE->autoflush(1);
-RPCREAD->autoflush(1);
-RESULTREAD->autoflush(1);
+socketpair(CHILD, PARENT, AF_UNIX, SOCK_DGRAM, PF_UNSPEC);
+CHILD->autoflush(1);
+PARENT->autoflush(1);
 
 if (fork() > 0) {
-  parent()
+  close(PARENT);
+  parent();
 } else {
+  close(CHILD);
   child();
 }
 
@@ -37,34 +36,33 @@ sub parent {
 
 sub check {
   my ($word) = @_;
-  print RPCWRITE "$word\n";
-  print RPCWRITE "$subre\n";
+  print CHILD "$word\n";
+  print CHILD "$subre\n";
+
+  sleep(1);
   print STDERR "parent reading  from result\n";
-  my $result; # = <RESULTREAD>;
+  my $result = <CHILD>;
   print STDERR "parent reading  complete\n";
   print STDERR "Result: $result\n";
   return "(bar)";
 }
 
 sub child {
-  RPCWRITE->autoflush(1);
-  RESULTWRITE->autoflush(1);
-  RPCREAD->autoflush(1);
-  RESULTREAD->autoflush(1);
-
   $SIG{PIPE} = sub { print STDERR "child got sigpipe\n"; exit(1) };
+
   while (1) {
     print STDERR "Child loop start\n";
-    chomp(my $word = <RPCREAD>);
-    chomp(my $subre = <RPCREAD>);
+    chomp(my $word = <PARENT>);
+    chomp(my $subre = <PARENT>);
     print "$word / $subre\n";
     if ($word =~ m/$subre/) {
       print STDERR "Sub re match\n";
-      print RESULTWRITE "\n";
+      print PARENT "nothing\n";
     } else {
       print STDERR "Sub re no match\n";
-      print RESULTWRITE "$stopre\n";
+      print PARENT "$stopre\n";
     }
-    die "Child suicide";
+    close(PARENT);
+    die;
   }
 }
