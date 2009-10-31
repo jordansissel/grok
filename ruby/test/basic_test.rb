7a6d6b8158112fd463cee2722f2b934d5c45ee05
Jordan Sissel <jls@semicomplete.com>
2009-10-31 21:02:32 +0000

    - use Test::Unit, rspec is too hard to use for these tests.
    git-svn-id: https://semicomplete.googlecode.com/svn/grok@2362 66067f73-fe4c-0410-82e9-b9c6d0c95a22

---
+++ ruby/test/basic_test.rb
@@ -0,0 +1,48 @@
+require 'rubygems'
+require 'Grok'
+require 'test/unit'
+
+class GrokBasicTests < Test::Unit::TestCase
+  def setup
+    @grok = Grok.new
+  end
+
+  def test_grok_methods
+    assert_respond_to(@grok, :compile)
+    assert_respond_to(@grok, :match)
+    assert_respond_to(@grok, :expanded_pattern)
+    assert_respond_to(@grok, :pattern)
+  end
+
+  def test_grok_compile_fails_on_invalid_expressions
+    bad_regexps = ["[", "[foo", "?", "foo????", "(?-"]
+    bad_regexps.each do |regexp|
+      assert_raise ArgumentError do
+        @grok.compile(regexp)
+      end
+    end
+  end
+
+  def test_grok_compile_succeeds_on_valid_expressions
+    good_regexps = ["[hello]", "(test)", "(?:hello)", "(?=testing)"]
+    good_regexps.each do |regexp|
+      assert_nothing_raised do
+        @grok.compile(regexp)
+      end
+    end
+  end
+
+  def test_grok_pattern_is_same_as_compile_pattern
+    pattern = "Hello world"
+    @grok.compile(pattern)
+    assert_equal(pattern, @grok.pattern)
+  end
+
+  # TODO(sissel): Move this test to a separate test suite aimed
+  # at testing grok internals
+  def test_grok_expanded_pattern_works_correctly
+    @grok.add_pattern("test", "hello world")
+    @grok.compile("%{test}")
+    assert_equal("(?<0000>hello world)", @grok.expanded_pattern)
+  end
+end
