df6fe7c4d917aabe9e2459ea111a38d3124d8c05
Jordan Sissel <jls@semicomplete.com>
2010-04-17 00:14:58 +0000

    - Add better library/header searching
    git-svn-id: https://semicomplete.googlecode.com/svn/grok@2819 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- ruby/ext/extconf.rb
+++ ruby/ext/extconf.rb
@@ -1,7 +1,6 @@
 require "mkmf"
 find_header("tcutil.h", "/usr/local/include")
 find_header("pcre.h", "/usr/local/include")
-find_header("grok.h", "../")
-find_library("grok", "grok_init", "../")
-
+find_header("grok.h", "/usr/local/include", "../../")
+find_library("grok", "grok_init", "../", "/usr/local/lib")
 create_makefile("Grok")
