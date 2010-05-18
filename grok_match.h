13ac921e840538fd9460028620b9f7bad92ea721
Jordan Sissel <jls@semicomplete.com>
2010-05-18 08:04:06 +0000

    - Add docs
    git-svn-id: https://semicomplete.googlecode.com/svn/grok@2871 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- grok_match.h
+++ grok_match.h
@@ -1,12 +1,28 @@
+/**
+ * @file grok_match.h
+ */
 #ifndef _GROK_MATCH_H_
 #define _GROK_MATCH_H_
 
 #include "grok_capture_xdr.h"
 
 typedef struct grok_match {
+  /** The grok_t instance that generated this match.
+   * This is a pointer to the grok parameter given to grok_exec().
+   * @see grok_exec()
+   */
   const grok_t *grok;
+
+  /** Pointer to the string matched.
+   * This is a pointer to the text parameter given to grok_exec() 
+   * @see grok_exec()
+   */
   const char *subject;
+
+  /** Start position of the match. 0 is beginning of string */
   int start;
+
+  /** End position of match. */
   int end;
 } grok_match_t;
 
