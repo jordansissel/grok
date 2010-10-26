4c6ac87d2fb17c40ac8acc9a3fb153fdf26f6e36
Jordan Sissel <jls@semicomplete.com>
2010-10-26 15:50:21 +0000

    - add example of compiling with -g
    git-svn-id: https://semicomplete.googlecode.com/svn/grok@3077 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- ruby/ext/extconf.rb
+++ ruby/ext/extconf.rb
@@ -4,3 +4,8 @@ find_header("pcre.h", "/usr/local/include")
 find_header("grok.h", "/usr/local/include", "../../")
 find_library("grok", "grok_init", "../", "../../", "/usr/local/lib")
 create_makefile("Grok")
+#with_cflags("-g") do
+  #with_ldflags("-g") do
+    #create_makefile("Grok")
+  #end
+#end
