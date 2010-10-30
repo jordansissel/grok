b238db87e9b01a077448a4252a73ef323b876d28
Jordan Sissel <jls@semicomplete.com>
2010-10-30 22:41:48 +0000

    - abort if we can't find requirements
    git-svn-id: https://semicomplete.googlecode.com/svn/grok@3084 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- ruby/ext/extconf.rb
+++ ruby/ext/extconf.rb
@@ -1,9 +1,19 @@
 require "mkmf"
-find_header("tcutil.h", "/usr/local/include")
-find_header("pcre.h", "/usr/local/include")
-find_header("grok.h", "/usr/local/include", "../../")
-find_library("grok", "grok_init", "../", "../../", "/usr/local/lib")
+
+# TODO(sissel): I don't think we need these two headers.
+#if !find_header("tcutil.h", "/usr/local/include")
+#if !find_header("pcre.h", "/usr/local/include")
+
+if !find_header("grok.h", "/usr/local/include", "../../")
+  raise "Could not find grok.h"
+end
+
+if !find_library("sgrok", "grok_init", "../", "../../", "/usr/local/lib")
+  raise "Could not find libgrok, is it installed?"
+end
+
 create_makefile("Grok")
+
 #with_cflags("-g") do
   #with_ldflags("-g") do
     #create_makefile("Grok")
