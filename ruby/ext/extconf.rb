ad8dab1576328dfdf7a65443f3ac0cac27e32275
Jordan Sissel <jls@semicomplete.com>
2010-11-15 19:15:44 +0000

    - look in /usr/lib and /usr/include
    git-svn-id: https://semicomplete.googlecode.com/svn/grok@3104 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- ruby/ext/extconf.rb
+++ ruby/ext/extconf.rb
@@ -4,11 +4,11 @@ require "mkmf"
 #if !find_header("tcutil.h", "/usr/local/include")
 #if !find_header("pcre.h", "/usr/local/include")
 
-if !find_header("grok.h", "/usr/local/include", "../../")
+if !find_header("grok.h", "/usr/local/include", "../../", "/usr/include")
   raise "Could not find grok.h"
 end
 
-if !find_library("grok", "grok_init", "../", "../../", "/usr/local/lib")
+if !find_library("grok", "grok_init", "../", "../../", "/usr/local/lib", "/usr/lib")
   raise "Could not find libgrok, is it installed?"
 end
 
