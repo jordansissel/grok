ef6a56b1e96de3cf737004dc51719d353dc2904c
Jordan Sissel <jls@semicomplete.com>
2011-08-20 01:57:21 -0700

    - clean up the sample

--- ruby/sample.rb
+++ ruby/sample.rb
@@ -1,5 +1,5 @@
-require "grok"
-require "pp"
+$: << File.join(File.dirname(__FILE__), "lib")
+require "grok-pure"
 
 patterns = {}
 
@@ -7,18 +7,16 @@ matches = [
   #"%{SYSLOGBASE} Accepted %{NOTSPACE:method} for %{DATA:user} from %{IPORHOST:client} port %{INT:port}",
   #"%{SYSLOGBASE} Did not receive identification string from %{IPORHOST:client}",
   #"%{SYSLOGBASE} error: PAM: authentication error for %{DATA:user} from %{IPORHOST:client}",
-  "%{SYSLOGBASE} .*"
-  #"%{COMBINEDAPACHELOG}",
+  #"%{QUOTEDSTRING}"
+  "%{COMBINEDAPACHELOG}",
   #"%{UNINDEXED}hello (?=%{GREEDYDATA})%{WORD}"
   
   #"( *%{DATA:key}:%{NOTSPACE:value})+"
 ]
 
 pile = Grok::Pile.new
-pile.add_patterns_from_file("../patterns/base")
+pile.add_patterns_from_file("patterns/pure-ruby/base")
 matches.collect do |m|
-  #g = Grok.new
-  #g.add_patterns_from_file("../patterns/base")
   pile.compile(m)
 end
 
@@ -27,12 +25,6 @@ time_start = Time.now.to_f
 $stdin.each do |line|
   grok, m = pile.match(line)
   if m
-    #data = Hash.new { |h,k| h[k] = Array.new }
-    #m.each_capture do |key, value|
-      #data[key] << value
-    #end
-    #pp data
-    #pp m.captures
     m.each_capture do |key, value|
       p key => value
     end
